1642f7cb5655d7e4b442bc940a9ae66db0b825bd
mayug <31482829+mayug@users.noreply.github.com>
2019-03-28 16:53:11 +0530

    Update cycle_gan_model.py

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -118,7 +118,7 @@ class CycleGANModel(BaseModel):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.input = torch.cat([self.real_A, self.noise_A], dim=1)
         
-        print('input shape ', self.input.shape)
+#         print('input shape ', self.input.shape)
         self.fake_B = self.netG_A(self.input)
 #         self.fake_B = self.netG_A(self.real_A)  # G_A(A)
         self.rec_A = self.netG_B(self.fake_B)   # G_B(G_A(A))
